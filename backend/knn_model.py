020e354de027b3b5d095f2fa7ac7ed9a0b539ede
AtharshanKen <atharshan.kennedy@dcmail.ca>
2026-04-12 21:16:31 -0400

    Cleaned my code and group memebers code of un used comments and added comments where needed

--- backend/knn_model.py
+++ backend/knn_model.py
@@ -11,7 +11,6 @@ def KNN_MD(NewRCat:list,dfscomb:pd.DataFrame,loc_id:str)->pd.DataFrame:
     df = dfscomb.copy()
     df.loc[len(df)] = NewRCat # Adding to the end for label encoding
     df = df.reset_index(drop=True)
-    # df = df[df['Location_ID'] != loc_id].reset_index(drop=True) # don't want to predict our self
     df1 = df.copy()
 
     # Encode categorical columns (except target)
@@ -28,7 +27,7 @@ def KNN_MD(NewRCat:list,dfscomb:pd.DataFrame,loc_id:str)->pd.DataFrame:
     df1['Day_Sin']   = df1['Date'].apply(lambda x: np.sin(2 * np.pi * (x.weekday()+1)/ 7))
     df1['Day_Cos']   = df1['Date'].apply(lambda x: np.cos(2 * np.pi * (x.weekday()+1)/ 7))
 
-    cols_to_use = [ # Not to be used but to tell what columns are being used
+    cols_to_use = [ # var Not to be used but to tell what columns are being used
         'PedsSen_Count',
         'Weather_Temperature',
         'Weather_Wind_Gust',
@@ -52,7 +51,8 @@ def KNN_MD(NewRCat:list,dfscomb:pd.DataFrame,loc_id:str)->pd.DataFrame:
     X = scaler.fit_transform(X)[-1] # Takes the last row that was added
     
     # Predict and get all locations not the selected location, store as [rows in dfscomb] in Found
-    yPD,yPI = knn_model.kneighbors([X])
+    yPD,yPI = knn_model.kneighbors([X]) # [[,,]],[[,,]], the second list contains row indexs to dfscomb 
+    # Predicition dist, Prediciton Index
     Found = pd.DataFrame(columns=df.columns)
 
     for i in range(len(yPI[0])):
--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI , HTTPException
-from typing import Literal
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 from pydantic.v1 import BaseSettings
-from pathlib import Path
 from dateutil import parser
 import pandas as pd
 import os
@@ -18,28 +16,27 @@ class Settings(BaseSettings):
 
 settings = Settings()
 
-api = os.environ.get("OPENAI_API_KEY")
+api = os.environ.get("OPENAI_API_KEY") # Switch between local/gcr
 if api is None:
     api = settings.openai_api_key
 client = AsyncOpenAI(api_key=api)
 
 # Model Exc File
-# from arima_model import ARIMA_MD 
 from knn_model import KNN_MD
 from xgb_model import XGB_MD
 
 folder1 = "./datasets"
-dfs_comb = pd.DataFrame()
+dfs_comb = pd.DataFrame() # Load both data sets together
 for df in [pd.read_csv(os.path.join(folder1,f)) for f in os.listdir(folder1) if f.endswith('daily.csv')]:
     dfs_comb = pd.concat([dfs_comb,df],axis='rows')
-dfs_comb['Date'] = dfs_comb['Date'].apply(lambda x: parser.parse(x).date())#pd.to_datetime(dfs_comb["Date"]).dt.strftime("%Y-%m-%d %H:%M:%S")#dfs_comb['Date'].apply(lambda x: parser.parse(x).date())#datetime.date(YYYY, MM, DD)
+dfs_comb['Date'] = dfs_comb['Date'].apply(lambda x: parser.parse(x).date())
 
 folder2 = "./datasets/flight_paths.csv"
-flights = pd.read_csv(folder2)
-flights['apt_time_dt_ds'] = flights['apt_time_dt_ds'].apply(lambda x: parser.parse(x).date())#pd.to_datetime(flights["apt_time_dt_ds"]).dt.strftime("%Y-%m-%d %H:%M:%S")#flights['apt_time_dt_ds'].apply(lambda x: parser.parse(x).date())#datetime.date(YYYY, MM, DD)
-flights['apt_time_dt_dp'] = flights['apt_time_dt_dp'].apply(lambda x: parser.parse(x).date())#pd.to_datetime(flights["apt_time_dt_dp"]).dt.strftime("%Y-%m-%d %H:%M:%S")#flights['apt_time_dt_dp'].apply(lambda x: parser.parse(x).date())
+flights = pd.read_csv(folder2) # Load Flight Data
+flights['apt_time_dt_ds'] = flights['apt_time_dt_ds'].apply(lambda x: parser.parse(x).date())
+flights['apt_time_dt_dp'] = flights['apt_time_dt_dp'].apply(lambda x: parser.parse(x).date())
 
-app = FastAPI()
+app = FastAPI() # Initiate the Fast API End Points
 
 def date_conv_to(df:pd.DataFrame,dates:list) -> list[dict]:
     df = df.copy()
@@ -60,36 +57,25 @@ def root():
 def backend_up():
     return {"status": "alive"}
 
-# def obtain_models():
-#     return 
-
 @app.post("/dfs_flgh_data")
-async def dfs_flgh_data():
+async def dfs_flgh_data(): # Return full flight data & historical data 
     return [date_conv_to(dfs_comb,['Date']),date_conv_to(flights,['apt_time_dt_ds','apt_time_dt_dp'])]
 
 class FrCtReq(BaseModel):
     loc:str
     lat:float
     long:float
-@app.post("/Forecasting")
+@app.post("/Forecasting") # Forecasting Function with XGB
 async def forecasting(input:FrCtReq):
-    # fc = ARIMA_MD(input.loc,input.lat,input.long)
     fc = XGB_MD(input.loc,input.lat,input.long)
-    # print(fc)
-    # for _,c in dfs_comb[(dfs_comb['Location_ID'] == input.loc)&(pd.to_datetime(dfs_comb['Date'],format="%Y-%m-%d").dt.year >= 2025)].reset_index(drop=True).loc[0:365,['Date','PedsSen_Count']].iterrows():
-    #     print(_,c)
-    #     indx = fc.index[fc['Date']==pd.Timestamp(c['Date'])].tolist()[0]
-    #     fc.loc[indx,'PedsSen_Count'] = c['PedsSen_Count']
     return date_conv_to(fc,['Date'])
 
 class RecReq(BaseModel):
     NewR:list
-    # main:str
     loc:str
-@app.post("/Recommendation")
+@app.post("/Recommendation") # Alt Dest with KNN model
 async def recommendation(input:RecReq):
     input.NewR[8] = datetime.date.fromisoformat(input.NewR[8])
-    # df = date_conv_from(pd.read_json(input.main),['Date'])
     kn = KNN_MD(input.NewR,dfs_comb,input.loc)
     kn['Date'] = pd.to_datetime(kn['Date'], errors="coerce").strftime("%Y-%m-%d %H:%M:%S")
     return kn.to_dict()
@@ -97,16 +83,12 @@ async def recommendation(input:RecReq):
 
 class open_AI(BaseModel):
     content:str 
-@app.post("/OPENAI")
+@app.post("/OPENAI") # Open AI Requester
 async def openai_api(input:open_AI):
     try:
         resp =  await client.responses.create(
                 model="gpt-4o-mini",
                 input = input.content,
-                # messages=[{
-                #     "role":input.role,
-                #     "content":input.content
-                # }],
                 temperature=0.2,
                 tools=[{"type": "web_search"}]
         )
